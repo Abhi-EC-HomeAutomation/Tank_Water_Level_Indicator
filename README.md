1dc682d3b0098e07b5263f7d5a15a95622981523
Abhijit Chavan <abhijeetec@gmail.com>
2020-12-24 01:02:13 +0530

    Added code and started writing the readme doc

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Tank_Water_Level_Indicator
+
+The final version of this project aims to create a level indicator system for water in the overhead tank and start/stop the motor as per set level. Also messaging system will be implemented to send the status to central server which will also send command to start/stop the motor remotely. 
+
+Some more features will be:
+1. led indicator for different functions.
+2. fail safe mechanism for stopping the motor to prevent water overflow in case of sensor faiilure.
+3. distributing tasks among the cores so that the sensor readings are not interrupted.
+
+![Alt text](Circuit_Diagram.jpg?raw=true "Title")
+
+##Components needed:
+1. ESP32 Dev board
+2. JSN SR04T module
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,41 @@
 #include <Arduino.h>
+#include <NewPing.h>
+
+//Defining pin
+//JSN-SR04T-2.0
+#define TRIGGER_PIN 32    // Arduino pin tied to trigger pin on the ultrasonic sensor. --Orange
+#define ECHO_PIN 33       // Arduino pin tied to echo pin on the ultrasonic sensor.  --Yellow
+
+//defining constants
+//JSN-SR04T-2.0
+const int MAX_DISTANCE = 200; // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+
+//JSN-SR04T-2.0
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+
+//Variables
+int sonarValue = 0;
+
+
+//defining empty functions
+int getWaterLevel();
+
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  sonarValue = getWaterLevel();
+  Serial.println(sonarValue);
+
+  delay(1000);
+}
+
+int getWaterLevel() {
+  return sonar.ping_cm();
 }
